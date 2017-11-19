f8c63071f3b678e3913ada28ca46e9f5dd610700
jeremy yon <jeremy yon>
2017-11-19 17:51:41 +0000

    usage of json database file

--- Wheatley.go
+++ Wheatley.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
     "net/http"
+    "regexp"
     // "text/template"
     // "time"
     // "math/rand"
     //  "io/ioutil"
     // "encoding/json"
     // "log"
+    "encoding/json"
+    "io/ioutil"
+    "os"
 )
 
 type Message struct {  
@@ -17,6 +21,12 @@ type Message struct {
 }
 type test_struct struct {
     Test string
+}
+
+type Page struct {
+    Reg    string    `json:"reg"`
+    Resp string `json:"resp"`
+    
 }
 
 
@@ -29,8 +39,42 @@ type test_struct struct {
 
 func wheatleyResponse(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<li>Hello, %s!</li>", r.URL.Query().Get("value")) //.Path[1:])
+
+
+ pages := getPages()
+    for _, p := range pages {
+
+        if ((regexp.MustCompile(p.Reg)).MatchString(r.URL.Query().Get("value")) ){
+  	        fmt.Fprintf(w, "<li>%s</li>", p.Resp) //.Path[1:])
+            return 
+        }        
+    }
+
+    fmt.Fprintf(w,"hi" + r.URL.Query().Get("value"))
+
+    fmt.Fprintf(w, "<li>Sorry, I don't quite get what you mean. Would you like me to look it up?</li>")
+
+
+    
 }
+
+
+
+func (p Page) toString() string {
+    return toJson(p)
+}
+
+func toJson(p interface{}) string {
+    bytes, err := json.Marshal(p)
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+
+    return string(bytes)
+}
+
+
 
 func main() {
 
@@ -39,4 +83,17 @@ func main() {
 
 	http.HandleFunc("/user-input", wheatleyResponse)
 	http.ListenAndServe(":8080", nil)
+}
+
+
+func getPages() []Page {
+    raw, err := ioutil.ReadFile("WheatleyDatabase.json")
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+
+    var c []Page
+    json.Unmarshal(raw, &c)
+    return c
 }
